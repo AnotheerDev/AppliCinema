fbb7eaef49772303c2faa329cc398f63904dd791
AnotheerDev <leger.julien94@gmail.com>
2023-05-30 16:58:32 +0200

    mise en place des fichers

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+
+use Controller\CinemaController;
+
+spl_autoload_register(function ($class_name) {
+    include $class_name . '.php';
+});
+
+// $ctrlCinema = new CinemaController();
+// if(isset($_GET["action"])) {
+//     switch ($_GET["action"]) {
+        
+//     }
+// }
