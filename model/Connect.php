cda24e2d963f7f09fe85352cdab38ebcd97ffd1a
AnotheerDev <leger.julien94@gmail.com>
2023-05-30 17:15:34 +0200

    créaction du fichier Connect.php

---
+++ model/Connect.php
@@ -0,0 +1,23 @@
+<?php
+
+use Controller\cinemaController;
+
+abstract class Connect{
+
+    const HOST = "localhost";
+    const DB = "cinema";
+    const USER = "root";
+    const PASS = "";
+
+    public static function seConnecter(){
+        try {
+            return new \PDO(
+                "mysql:host=" . self::HOST . ";dbname=" . self::DB . ";charset=utf8",
+                self::USER,
+                self::PASS
+            );
+        } catch (\PDOException $ex) {
+            return $ex->getMessage();
+        }
+    }
+}
