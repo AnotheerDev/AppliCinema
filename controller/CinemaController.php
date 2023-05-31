ed81f9d6feb4e4e7f25449a3ecb8ae789f7169c7
AnotheerDev <leger.julien94@gmail.com>
2023-05-31 10:39:21 +0200

    ajout function showHome & showFilms

--- controller/CinemaController.php
+++ controller/CinemaController.php
@@ -1,3 +1,34 @@
 <?php
 
-namespace CinemaController;
+namespace Controller;
+use Model\Connect;
+
+class CinemaController {
+
+
+    public function showHome() {
+        // Logique pour afficher la vue "home"
+        include 'view/home.php';
+    }
+
+    public function showFilms() {
+        // Logique pour afficher la vue "films"
+        include 'view/films.php';
+    }
+
+
+    public function listFilms() {
+        $pdo = Connect::seConnecter();
+        $requete = $pdo -> query("
+            SELECT titre, dateSortie
+            FROM film
+        ");
+        require "view/films.php";
+    }
+
+
+
+
+
+
+}
