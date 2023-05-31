b06c46c868df3c80a892ebc06b53aa8057987654
AnotheerDev <leger.julien94@gmail.com>
2023-05-31 11:09:32 +0200

    ajout du tableau de films dans la vue

--- view/films.php
+++ view/films.php
@@ -3,6 +3,27 @@
     ob_start();
 ?>
 
+<p class="uk-label uk-label-warning">Il y a <?= $requete->rowCount() ?> films</p>
+
+<table class="uk-label uk-label-stripped">
+    <thead>
+        <tr>
+            <th>TITRE</th>
+            <th>ANNEE DE SORTIE</th>
+        </tr>
+    </thead>
+    <tbody>
+        <?php
+            foreach($requete->fetchAll() as $film) { ?>
+            <tr>
+                <td><?= $film["titre"]?></td>
+                <td><?= $film["dateSortie"]?></td>
+            </tr>
+            <?php } ?>
+    </tbody>
+</table>
+
+
 <?php
 
 $content = ob_get_clean();
