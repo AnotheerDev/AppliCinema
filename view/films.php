1e904909d2d276a7c9ce6314a6e95e1e2d7e6773
AnotheerDev <leger.julien94@gmail.com>
2023-06-01 11:30:16 +0200

    suppression des modals

--- view/films.php
+++ view/films.php
@@ -16,34 +16,13 @@ ob_start();
         <?php
         foreach ($requete->fetchAll() as $film) { ?>
             <tr>
-                <td><a href="#" data-toggle="modal" data-target="#filmModal"><?= $film["titre"] ?></a></td>
+                <td><?= $film["titre"] ?></td>
                 <td><?= $film["dateSortie"] ?></td>
             </tr>
         <?php } ?>
     </tbody>
 </table>
 
-<!-- MODAL -->
-<div class="modal fade" id="filmModal" tabindex="-1" role="dialog" aria-labelledby="filmModalLabel" aria-hidden="true">
-    <div class="modal-dialog" role="document">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title" id="filmModalLabel">Détails du film</h5>
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
-                </button>
-            </div>
-            <div class="modal-body">
-                <h2 id="filmModalTitre"></h2>
-                <p id="filmModalDate"></p>
-                <!-- Ajoutez d'autres éléments du contenu modal si nécessaire -->
-            </div>
-            <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-dismiss="modal">Fermer</button>
-            </div>
-        </div>
-    </div>
-</div>
 
 
 <?php
--- view/stars.php
+++ view/stars.php
@@ -15,34 +15,13 @@
         <?php
             foreach($requete->fetchAll() as $star) { ?>
             <tr>
-
-                <td><a href="#" data-toggle="modal" data-target="#personModal"><?= $star["nom"]?></a></td>
+                <td><?= $star["nom"]?></td>
             </tr>
             <?php } ?>
     </tbody>
 </table>
 
-<!-- MODAL -->
-<div class="modal fade" id="personModal" tabindex="-1" role="dialog" aria-labelledby="personModalLabel" aria-hidden="true">
-    <div class="modal-dialog" role="document">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title" id="personModalLabel">Détails du film</h5>
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
-                </button>
-            </div>
-            <div class="modal-body">
-                <h2 id="personModalTitre"></h2>
-                <p id="personModalDate"></p>
-                <!-- Ajoutez d'autres éléments du contenu modal si nécessaire -->
-            </div>
-            <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-dismiss="modal">Fermer</button>
-            </div>
-        </div>
-    </div>
-</div>
+
 
 <?php
 
