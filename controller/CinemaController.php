6f54ef4a12568b0439da52829e90f5cf8a0891d2
AnotheerDev <leger.julien94@gmail.com>
2023-06-01 12:04:43 +0200

    change query par prepare

--- controller/CinemaController.php
+++ controller/CinemaController.php
@@ -5,6 +5,7 @@ use Model\Connect;
 
 class CinemaController {
 
+    // En utilisant prepare() et execute() avec une requête préparée, vous bénéficiez des avantages de la sécurité offerte par les requêtes préparées, même si la requête elle-même ne contient pas de paramètres dynamiques. ( injection sql)
 
     public function showHome() {
         // Logique pour afficher la vue "home"
@@ -15,25 +16,38 @@ class CinemaController {
     public function showFilms() {
         $pdo = Connect::seConnecter();
         // var_dump($pdo);die;  
-        $requete = $pdo -> query("
+        $requete = $pdo -> prepare("
             SELECT titre, dateSortie
             FROM film
         ");
+        $requete->execute();
         require "view/films.php";
     }
 
 
     public function showStars() {
-        // Logique pour afficher la vue "home"
         $pdo = Connect::seConnecter();
-        $requete = $pdo -> query("
-            SELECT CONCAT (prenom,' ', nom) as nom
+        $requete = $pdo->prepare("
+            SELECT CONCAT(prenom, ' ', nom) AS nom
             FROM personne
         ");
+        $requete->execute();
         require 'view/stars.php';
     }
+    
 
 
-
+    public function showFilmDetails($id) {
+        // Logique pour obtenir les détails du film à partir de l'ID
+        $pdo = Connect::seConnecter();
+        $requete = $pdo->prepare("
+            SELECT *
+            FROM film
+            WHERE id = :id
+        ");
+        $requete->execute();
+        // Afficher la vue des détails du film
+        require 'view/filmDetails.php';
+    }
 
 }
