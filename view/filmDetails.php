59c0b85651357149f4c0b439568ef171df547e6e
AnotheerDev <leger.julien94@gmail.com>
2023-06-02 10:39:40 +0200

    ajout des détails du films

--- view/filmDetails.php
+++ view/filmDetails.php
@@ -2,6 +2,31 @@
 ob_start();
 ?>
 
+<table class="uk-label uk-label-stripped">
+    <thead>
+        <tr>
+            <th>TITRE</th>
+            <th>DATE DE SORTIE</th>
+            <th>DUREE</th>
+            <th>SINOPSIS</th>
+            <th>NOTE</th>
+            <th>REALISATEUR</th>
+        </tr>
+    </thead>
+    <tbody>
+        <?php
+        foreach ($requete->fetchAll() as $film) { ?>
+            <tr>
+                <td><?= $film["titre"] ?></td>
+                <td><?= $film["dateSortie"] ?></td>
+                <td><?= $film["duree"] ?></td>
+                <td><?= $film["synopsis"] ?></td>
+                <td><?= $film["note"] ?></td>
+                <td><?= $film["idRealisateur"] ?></td>
+            </tr>
+        <?php } ?>
+    </tbody>
+</table>
 <?php
 
 $content = ob_get_clean();
