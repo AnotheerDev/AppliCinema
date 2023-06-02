370a019c30006fb044882763c00405180444b078
AnotheerDev <leger.julien94@gmail.com>
2023-06-02 14:18:41 +0200

    precision si la personne est acteur&réalisateur

--- view/personneDetails.php
+++ view/personneDetails.php
@@ -5,6 +5,17 @@ ob_start();
 <h2><?= $personne['nom'] ?> <?= $personne['prenom'] ?></h2>
 <p>est un(e) : <?= $personne['sexe'] ?></p>
 <p>né(e) le : <?= $personne['dateNaissance'] ?></p>
+<p>est un(e) :
+    <?php if ($personne['idRealisateur'] && $personne['idActeur']) : ?>
+        Acteur et réalisateur
+    <?php elseif ($personne['idRealisateur']) : ?>
+        Réalisateur
+    <?php elseif ($personne['idActeur']) : ?>
+        Acteur
+    <?php else : ?>
+        Aucun rôle spécifié
+    <?php endif; ?>
+</p>
 
 
 
