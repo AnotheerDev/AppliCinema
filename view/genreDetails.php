d5040ba241517d1babc4ac9443987b3b4efe88c6
AnotheerDev <leger.julien94@gmail.com>
2023-06-05 11:08:24 +0200

    requete affichage film par genre

--- view/genreDetails.php
+++ view/genreDetails.php
@@ -2,11 +2,28 @@
 ob_start();
 ?>
 
-<a>ahahah</a>
+<h2><?= $genre['nom'] ?></h2>
 
+<h3>Films du genre <?= $genre['nom'] ?></h3>
+<ul>
+    <?php
+    foreach ($filmIds as $filmId) {
+        $requeteFilm = $pdo->prepare("
+            SELECT *
+            FROM film
+            WHERE idFilm = :filmId
+        ");
+        $requeteFilm->bindParam(':filmId', $filmId, \PDO::PARAM_INT);
+        $requeteFilm->execute();
+        $film = $requeteFilm->fetch();
+
+        // Afficher les détails du film
+        echo "<li>" . $film['titre'] . "</li>";
+    }
+    ?>
+</ul>
 
 <?php
-
 $content = ob_get_clean();
-$title = "Détails Genre";
+$title = "Détails du genre";
 require "template.php";
