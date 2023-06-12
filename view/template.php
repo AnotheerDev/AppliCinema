be340addeade7a92c711424124ec69381e7b1fa9
AnotheerDev <leger.julien94@gmail.com>
2023-06-12 09:43:21 +0200

    modification nav bar

--- view/template.php
+++ view/template.php
@@ -12,30 +12,52 @@
 </head>
 
 <body>
-    <ul class="nav justify-content-center">
-        <li class="nav-item">
-            <a class="nav-link active text-dark" aria-current="page" href="index.php?action=home">HOME</a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link active text-dark" aria-current="page" href="index.php?action=films">FILMS</a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link active text-dark" aria-current="page" href="index.php?action=stars">ACTEURS - REALISATEURS</a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link active text-dark" aria-current="page" href="index.php?action=genre">GENRE</a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link active text-dark" aria-current="page" href="index.php?action=ajoutStar">AJOUT STARS</a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link active text-dark" aria-current="page" href="index.php?action=addFilm">AJOUT FILM</a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link active text-dark" aria-current="page" href="index.php?action=casting">CASTING</a>
-        </li>
-    </ul>
 
+
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="#"></a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item active">
+                    <a class="nav-link" href="index.php?action=home">HOME <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item active">
+                    <a class="nav-link" href="index.php?action=films">FILMS <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item active">
+                    <a class="nav-link" href="index.php?action=stars">ACTEURS - REALISATEURS <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item active">
+                    <a class="nav-link" href="index.php?action=genre">GENRE <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item active">
+                    <a class="nav-link" href="index.php?action=casting">CASTING <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        AJOUT
+                    </a>
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <a class="dropdown-item" href="index.php?action=ajoutStar">AJOUT STARS</a>
+                        <a class="dropdown-item" href="index.php?action=addFilm">AJOUT FILM</a>
+                    </div>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        EDITION
+                    </a>
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <a class="dropdown-item" href="index.php?action=modifierPersonne">STARS</a>
+                        <a class="dropdown-item" href="#">FILMS</a>
+                    </div>
+                </li>
+            </ul>
+        </div>
+    </nav>
 
     <div class="container">
         <?= $content ?>
