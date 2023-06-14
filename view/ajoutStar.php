a57fd78059ca49d4a82d3815dd710fcca76aec85
AnotheerDev <leger.julien94@gmail.com>
2023-06-14 10:54:05 +0200

    ajout <br> mise en forme

--- view/ajoutStar.php
+++ view/ajoutStar.php
@@ -2,21 +2,21 @@
 ob_start();
 ?>
 
-<div>
+
     <p>Ajouter une nouvelle personne : </p>
 
     <form action="index.php?action=addPersonne" method="post">
-        <input type="text" name="nom" maxlength="50" placeholder="Nom">
-        <input type="text" name="prenom" maxlength="50" placeholder="Prenom">
-        <input type="text" name="sexe" maxlength="5" placeholder="Genre">
-        <input type="date" name="dateNaissance">
+        <input type="text" name="nom" maxlength="50" placeholder="Nom"><br>
+        <input type="text" name="prenom" maxlength="50" placeholder="Prenom"><br>
+        <input type="text" name="sexe" maxlength="5" placeholder="Genre"><br>
+        <input type="date" name="dateNaissance"><br>
         <label for="acteur">Acteur :</label>
         <input type="checkbox" name="acteur" id="acteur">
         <label for="realisateur">Réalisateur :</label>
-        <input type="checkbox" name="realisateur" id="realisateur">
+        <input type="checkbox" name="realisateur" id="realisateur"><br>
         <input type="submit" name="submit" value="Ajouter">
     </form>
-</div>
+
 
 <div>
     <p>Ajouter un nouveau rôle : </p>
