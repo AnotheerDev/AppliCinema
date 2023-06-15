0967f6876a56a6a8cfdb68e83de40dbff19da626
AnotheerDev <leger.julien94@gmail.com>
2023-06-15 20:46:31 +0200

    ajout star card

--- view/roleDetails.php
+++ view/roleDetails.php
@@ -3,18 +3,22 @@ ob_start();
 ?>
 
 <?php if (!empty($castings)) : ?>
-    <p> Voici tous les acteurs ayant joué le rôle de : <?= $castings[0]['nom'] ?></p>
+    <h3> Voici tous les acteurs ayant joué le rôle de : <?= $castings[0]['nom'] ?></h3>
 <?php else : ?>
-    <p>Aucun acteur n'a joué ce rôle.</p>
+    <h3>Aucun acteur n'a joué ce rôle.</h3>
 <?php endif; ?>
 
-<?php
-foreach ($castings as $casting) { ?>
-    <a href="index.php?action=personneDetails&id=<?= $casting["idPersonne"] ?>">
-        <p><?= $casting["actor"] ?></p>
-    </a>
-<?php } ?>
-
+<div class="star-container">
+    <?php
+    foreach ($castings as $casting) { ?>
+        <a href="index.php?action=personneDetails&id=<?= $casting["idPersonne"] ?>">
+            <div class="star-card">
+                <p><?= $casting["actor"] ?></p>
+                <img class="actor-affiche" src="public/upload/561836.jpg-c_310_420_x-f_jpg-q_x-xxyxx.jpg">
+            </div>
+        </a>
+    <?php } ?>
+</div>
 
 <?php
 
